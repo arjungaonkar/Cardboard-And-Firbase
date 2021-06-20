aef719cd2795b99910e3132f1ff00f7e29fa85e5
ArjunGaonkar <arjungnkr7@gmail.com>
2021-06-20 10:39:00 +0530

    Imported firebase firestore sdk and added a simple script

---
+++ Assets/Scripts/FirebaseTest.cs
@@ -0,0 +1,22 @@
+﻿using Firebase.Extensions;
+using Firebase.Firestore;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseTest : MonoBehaviour
+{
+    void Start()
+    {
+        DocumentReference docRef = FirebaseFirestore.DefaultInstance.Collection("cities").Document("LA");
+        Dictionary<string, object> city = new Dictionary<string, object>
+        {
+                { "Name", "Los Angeles" },
+                { "State", "CA" },
+                { "Country", "USA" }
+        };
+        docRef.SetAsync(city).ContinueWithOnMainThread(task =>
+        {
+            Debug.Log("Added data to the LA document in the cities collection.");
+        });
+    }
+}
